8f6e0d22303ebc17fc1084a7169f06c5e0aedc69
Tristan Penman <tristan@tristanpenman.com>
2026-04-04 22:09:18 +1100

    Finish periph motor driver

--- go.mod
+++ go.mod
@@ -15,6 +15,10 @@ require (
 	github.com/gofrs/uuid v4.0.0+incompatible // indirect
 	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/hashicorp/go-multierror v1.0.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sigurn/crc8 v0.0.0-20160107002456-e55481d6f45c // indirect
+	github.com/sigurn/utils v0.0.0-20190728110027-e1fefb11a144 // indirect
 	golang.org/x/net v0.50.0 // indirect
 	golang.org/x/sync v0.17.0 // indirect
+	periph.io/x/periph v3.6.2+incompatible // indirect
 )
--- pkg/motor/periph_driver.go
+++ pkg/motor/periph_driver.go
@@ -3,13 +3,28 @@ package motor
 import (
 	"context"
 	"fmt"
+	"math"
 	"sync"
 
+	"periph.io/x/conn/v3/gpio"
 	"periph.io/x/conn/v3/i2c"
 	"periph.io/x/conn/v3/i2c/i2creg"
 	"periph.io/x/devices/v3/pca9685"
 	"periph.io/x/host/v3"
 )
+
+type periphMotorChannel struct {
+	pwm int
+	in1 int
+	in2 int
+}
+
+var periphMotorChannels = [4]periphMotorChannel{
+	{pwm: 8, in1: 10, in2: 9},
+	{pwm: 13, in1: 11, in2: 12},
+	{pwm: 2, in1: 4, in2: 3},
+	{pwm: 7, in1: 5, in2: 6},
+}
 
 type PeriphDriver struct {
 	mu        sync.Mutex
@@ -56,10 +71,75 @@ func NewPeriphDriverWithThreshold(threshold float64) (*PeriphDriver, error) {
 	return driver, nil
 }
 
+func (d *PeriphDriver) setChannel(channel int, on bool) error {
+	if on {
+		return d.pca.SetFullOn(channel)
+	}
+	return d.pca.SetFullOff(channel)
+}
+
+func (d *PeriphDriver) setMotor(motor int, throttle float64) error {
+	if motor < 1 || motor > len(periphMotorChannels) {
+		return fmt.Errorf("unsupported motor index %d", motor)
+	}
+	ch := periphMotorChannels[motor-1]
+
+	t := math.Max(-1, math.Min(1, throttle))
+	duty := gpio.Duty(math.Round(math.Abs(t) * float64(gpio.DutyMax)))
+
+	switch {
+	case t > 0:
+		if err := d.setChannel(ch.in1, true); err != nil {
+			return fmt.Errorf("set in1 high: %w", err)
+		}
+		if err := d.setChannel(ch.in2, false); err != nil {
+			return fmt.Errorf("set in2 low: %w", err)
+		}
+	case t < 0:
+		if err := d.setChannel(ch.in1, false); err != nil {
+			return fmt.Errorf("set in1 low: %w", err)
+		}
+		if err := d.setChannel(ch.in2, true); err != nil {
+			return fmt.Errorf("set in2 high: %w", err)
+		}
+	default:
+		if err := d.setChannel(ch.in1, false); err != nil {
+			return fmt.Errorf("release in1: %w", err)
+		}
+		if err := d.setChannel(ch.in2, false); err != nil {
+			return fmt.Errorf("release in2: %w", err)
+		}
+	}
+
+	if duty == 0 {
+		return d.pca.SetFullOff(ch.pwm)
+	}
+	return d.pca.SetPwm(ch.pwm, 0, duty)
+}
+
+func (d *PeriphDriver) Stop(context.Context) error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	for motor := 1; motor <= 4; motor++ {
+		if err := d.setMotor(motor, 0); err != nil {
+			return fmt.Errorf("stop motor %d: %w", motor, err)
+		}
+	}
+	return nil
+}
+
 func (d *PeriphDriver) Forwards(context.Context) error {
 	d.mu.Lock()
 	defer d.mu.Unlock()
 
+	values := [4]float64{-1, 1, -1, 1}
+	for idx, value := range values {
+		motor := idx + 1
+		if err := d.setMotor(motor, value); err != nil {
+			return fmt.Errorf("forwards motor %d: %w", motor, err)
+		}
+	}
 	return nil
 }
 
@@ -67,6 +147,13 @@ func (d *PeriphDriver) Backwards(context.Context) error {
 	d.mu.Lock()
 	defer d.mu.Unlock()
 
+	values := [4]float64{1, -1, 1, -1}
+	for idx, value := range values {
+		motor := idx + 1
+		if err := d.setMotor(motor, value); err != nil {
+			return fmt.Errorf("backwards motor %d: %w", motor, err)
+		}
+	}
 	return nil
 }
 
@@ -74,6 +161,11 @@ func (d *PeriphDriver) SpinCW(context.Context) error {
 	d.mu.Lock()
 	defer d.mu.Unlock()
 
+	for motor := 1; motor <= 4; motor++ {
+		if err := d.setMotor(motor, 1); err != nil {
+			return fmt.Errorf("spin_cw motor %d: %w", motor, err)
+		}
+	}
 	return nil
 }
 
@@ -81,13 +173,11 @@ func (d *PeriphDriver) SpinCCW(context.Context) error {
 	d.mu.Lock()
 	defer d.mu.Unlock()
 
-	return nil
-}
-
-func (d *PeriphDriver) Stop(context.Context) error {
-	d.mu.Lock()
-	defer d.mu.Unlock()
-
+	for motor := 1; motor <= 4; motor++ {
+		if err := d.setMotor(motor, -1); err != nil {
+			return fmt.Errorf("spin_ccw motor %d: %w", motor, err)
+		}
+	}
 	return nil
 }
 
@@ -95,7 +185,24 @@ func (d *PeriphDriver) Throttle(_ context.Context, value float64) (bool, error) 
 	d.mu.Lock()
 	defer d.mu.Unlock()
 
-	return false, nil
+	if math.Abs(value) <= d.threshold {
+		for motor := 1; motor <= 4; motor++ {
+			if err := d.setMotor(motor, 0); err != nil {
+				return false, fmt.Errorf("threshold stop motor %d: %w", motor, err)
+			}
+		}
+		return false, nil
+	}
+
+	values := [4]float64{value, -value, value, -value}
+	for idx, throttle := range values {
+		motor := idx + 1
+		if err := d.setMotor(motor, throttle); err != nil {
+			return false, fmt.Errorf("throttle motor %d: %w", motor, err)
+		}
+	}
+
+	return true, nil
 }
 
 func (d *PeriphDriver) Close(context.Context) error {
